02bd0f342bd461a7e0c8cae9d9585a177dd44962
tutaurelia <tutaurelia@outlook.com>
2015-12-12 22:54:32 +0100

    Part6 Completed
    Source Files Part 6 completed

---
+++ Part6Completed/src/views/contacts-list.ts
@@ -0,0 +1,36 @@
+import {inject} from "aurelia-framework";
+import {HttpClient} from "aurelia-fetch-client";
+
+@inject(HttpClient)
+export class ContaqctsList {
+  searchEntry = "";
+  contacts = [];
+  contactId = "";
+  contact = "";
+  currentPage = 1;
+  textShowAll = "Show All";
+
+
+  constructor(private http : HttpClient) {
+    this.http = http;
+  }
+
+  updateContacts() {
+      return this.http.fetch("contacts?query=" + this.searchEntry).then(response => response.json()).then(data => {
+        this.contacts = (<any> data).contacts;
+      });
+  }
+
+  get canSearch() {
+    return (this.searchEntry !== "");
+  }
+
+  activate() {
+    return this.updateContacts();
+  }
+
+  displayAllContacts() {
+    this.searchEntry = "";
+    this.activate();
+  }
+}
