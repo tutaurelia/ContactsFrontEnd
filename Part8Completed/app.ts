2241db33525f0e4b2cedd41edc2f8789c045ae9b
tutaurelia <tutaurelia@outlook.com>
2015-12-25 00:08:24 +0100

    Part8Completed
    Part8Completed

---
+++ Part8Completed/app.ts
@@ -0,0 +1,30 @@
+import {inject} from "aurelia-framework";
+import {Router} from "aurelia-router";
+import {HttpClient} from "aurelia-fetch-client";
+
+@inject(HttpClient, Router)
+export class App {
+    router: Router;
+    http: HttpClient;
+
+    constructor(http: HttpClient) {
+        console.log("content type - defaults 3");
+        http.configure(config => {
+            config
+                .withBaseUrl("http://tutaurelia.azurewebsites.net/api/")
+        });
+        this.http = http;
+    }
+
+    configureRouter(config, router: Router) {
+        this.router = router;
+
+        config.title = "Contacts App";
+        config.map([
+            { route: ["", "contactslist"], moduleId: "./views/contacts-list", nav: true, title: "List" },
+            { route: "tutaurelia", moduleId: "./views/tutaurelia", nav: true, title: "tutaurelia.net" },
+            { route: "contacts/*id", moduleId: "./views/edit", title: "Edit Contact" },
+            { route: "insert", moduleId: "./views/insert", title: "Insert Contact" }
+        ]);
+    }
+}
