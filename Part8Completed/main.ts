1a6a3a8b4ac5c345d16c8b64182bec11e425a744
tutaurelia <tutaurelia@outlook.com>
2016-01-04 22:09:00 +0100

    Part8Completed
    index.html - main.ts -> setup aurelia-dialog

---
+++ Part8Completed/main.ts
@@ -0,0 +1,7 @@
+export function configure(aurelia) {
+    aurelia.use
+      .standardConfiguration()
+      .developmentLogging()
+      .plugin("aurelia-dialog");
+    aurelia.start().then(a => a.setRoot());
+  }
